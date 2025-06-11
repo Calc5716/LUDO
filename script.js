fdfd47e56001d58150737570c6b90344903fb67b
Paramesh Bandhu Banerjee <parameshbandhubanerjee@gmail.com>
2025-06-11 09:34:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-let squares = localStorage.getItem("squares");
-let pieces = localStorage.getItem("pieces");
+// let squares = localStorage.getItem("squares");
+// let pieces = localStorage.getItem("pieces");
 
-squares = parseInt(squares);
-pieces = parseInt(pieces);
+// squares = parseInt(squares);
+// pieces = parseInt(pieces);
 
-const redSpans = document.querySelectorAll('#redBoard span i');
-const blueSpans = document.querySelectorAll('#blueBoard span i');
-const greenSpans = document.querySelectorAll('#greenBoard span i');
-const yellowSpans = document.querySelectorAll('#yellowBoard span i');
+// const redSpans = document.querySelectorAll('#redBoard span i');
+// const blueSpans = document.querySelectorAll('#blueBoard span i');
+// const greenSpans = document.querySelectorAll('#greenBoard span i');
+// const yellowSpans = document.querySelectorAll('#yellowBoard span i');
 
-for (let i = 0; i < (4-pieces); i++) {
-    redSpans[i].remove();
-    yellowSpans[i].remove();
-    blueSpans[i].remove();
-    greenSpans[i].remove();
-}
+// for (let i = 0; i < (4-pieces); i++) {
+//     redSpans[i].remove();
+//     yellowSpans[i].remove();
+//     blueSpans[i].remove();
+//     greenSpans[i].remove();
+// }
